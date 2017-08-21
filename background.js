9d0797d7670c790ec4a2a326d8534ad77065e1b6
YFdyh000 <yfdyh000@gmail.com>
2017-08-22 05:38:40 +0800

    Allow "Close Tabs to the Left" from context menus

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
-browser.browserAction.onClicked.addListener(async (activeTab) => {
-    const tabs = await browser.tabs.query({ currentWindow: true });
-    for (const tab of tabs.reverse()) {
-        if (tab.id === activeTab.id) break;
+async function closeTabsTo(currentTab, directionLeft = false) {
+    let tabs = await browser.tabs.query({ currentWindow: true });
+    if (!directionLeft) tabs = tabs.reverse()
+    for (const tab of tabs) {
+        if (tab.id === currentTab.id) break;
         browser.tabs.remove(tab.id);
     }
+}
+browser.browserAction.onClicked.addListener((activeTab) => {
+    closeTabsTo(activeTab, false);
+});
+browser.contextMenus.create({
+  id: "closeTabsToLeft",
+  title: browser.i18n.getMessage("closeTabsToLeft"),
+  contexts: ["browser_action"],
+  onclick: async () => {
+      const [activeTab] = await browser.tabs.query({ currentWindow: true, active: true });
+      closeTabsTo(activeTab, true);
+  }
 });

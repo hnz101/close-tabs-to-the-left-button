76525da774c5276cb1590f0aa4f59e37883f2fd7
YFdyh000 <yfdyh000@gmail.com>
2017-08-22 05:53:56 +0800

    Show a notification after tabs removed

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
 async function closeTabsTo(currentTab, directionLeft = false) {
     let tabs = await browser.tabs.query({ currentWindow: true });
     if (!directionLeft) tabs = tabs.reverse()
+    let number = 0;
     for (const tab of tabs) {
         if (tab.id === currentTab.id) break;
         browser.tabs.remove(tab.id);
+        number += 1;
     }
+    browser.notifications.create({
+        "type": "basic",
+        "title": browser.i18n.getMessage("extensionName"),
+        "message": browser.i18n.getMessage("closedNumberNotification", number)
+    });
 }
 browser.browserAction.onClicked.addListener((activeTab) => {
     closeTabsTo(activeTab, false);

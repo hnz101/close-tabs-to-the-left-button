ee1d023499f6d653e1271338ce50e8f9b35f294d
hnz101 <patrik.heinz@gmail.com>
2017-11-15 11:34:26 +0100

    Changed everything to close left instead of right tabs

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
-async function closeTabsTo(currentTab, directionLeft = false) {
+async function closeTabsTo(currentTab, directionLeft) {
     let tabs = await browser.tabs.query({ currentWindow: true });
     if (!directionLeft) tabs = tabs.reverse()
     for (const tab of tabs) {
         if (tab.id === currentTab.id) break;
-        browser.tabs.remove(tab.id);
+		if (!tab.pinned) {
+			browser.tabs.remove(tab.id);
+		}
     }
 }
 browser.browserAction.onClicked.addListener((activeTab) => {
-    closeTabsTo(activeTab, false);
+    closeTabsTo(activeTab, true);
 });
 browser.contextMenus.create({
   id: "closeTabsToLeft",
